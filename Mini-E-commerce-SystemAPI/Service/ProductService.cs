e5128ab909acf75dae8b089034193a852f06b1e0
sibin <sibindas1806@gmail.com>
2024-12-20 12:30:30 +0530

    ProductService--- refined the readablity of the service

--- Mini-E-commerce-SystemAPI/Service/ProductService.cs
+++ Mini-E-commerce-SystemAPI/Service/ProductService.cs
@@ -14,15 +14,18 @@ namespace Mini_E_commerce_SystemAPI.Service
             _dbConnection = dbConnection;
 
         }
+        //Services for fetching all the products from database 
         public async Task<IEnumerable<Product>> GetAllProductsAsync()
         {
             return await _dbConnection.QueryAsync<Product>("SELECT * FROM Products");
         }
-
+        //Services for fetching a particular product from database 
         public async Task<Product> GetProductByIdAsync(int id)
         {
             return await _dbConnection.QueryFirstOrDefaultAsync<Product>("SELECT * FROM Products WHERE ProductId = @Id", new { Id = id });
         }
+
+        //services to Add a new product into database 
         public async Task AddProductAsync([FromBody] Product product)
         {
             await _dbConnection.ExecuteAsync("Insert into products (name,Description,Price,StockQuantity)values (@name,@Description,@Price,@StockQuantity)", product);
