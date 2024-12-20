b30f0ea7d43270b0889719f5169472d93ccded01
sibin <sibindas1806@gmail.com>
2024-12-20 12:34:59 +0530

    ShoppingCartService--- improved the readablity of the service

--- Mini-E-commerce-SystemAPI/Service/ShoppingCartService.cs
+++ Mini-E-commerce-SystemAPI/Service/ShoppingCartService.cs
@@ -13,18 +13,18 @@ namespace Mini_E_commerce_SystemAPI.Service
         {
             _dbConnection = dbConnection;
         }
-
+        // Service for Adding products into the shoppingcart 
         public async Task AddToCartAsync(ShoppingCartItem item)
         {
             await _dbConnection.ExecuteAsync("INSERT INTO ShoppingCart (UserId, Quantity,price) VALUES (@UserId, @Quantity, @price)", item);
 
         }
-
+        //Service for deleting cart from the shopping cart 
         public async Task RemoveFromCartAsync(ShoppingCartItem item)
         {
             await _dbConnection.ExecuteAsync("DELETE FROM ShoppingCart WHERE CartId = @CartId", item);
         }
-
+        //Service for feting the shoppingcart details of a particular user 
         public async Task<IEnumerable<ShoppingCartItem>> GetCartItemsAsync(int userId)
         {
             return await _dbConnection.QueryAsync<ShoppingCartItem>("SELECT * FROM ShoppingCart WHERE UserId = @UserId", new { UserId = userId });
